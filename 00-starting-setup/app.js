13896646c5cb1c859edf8eac9221665d06ca8e36
Prateek Sarangi <sarangiprateek80@gmail.com>
2020-06-04 01:38:30 +0530

    Pug files added

--- 00-starting-setup/app.js
+++ 00-starting-setup/app.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.engine('pug', require('pug').__express) // Not written in the course.
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
