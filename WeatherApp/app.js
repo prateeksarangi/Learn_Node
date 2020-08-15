0b46889277538ff657f692d3c368ef61a5832e14
Prateek Sarangi <sarangiprateek80@gmail.com>
2020-08-16 04:24:31 +0530

    Basic weather details added to the app.

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,9 +1,11 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=d28ac7afbe74e9246448e2a4c74d2b23&query=37.8267,-122.4233';
+const url = 'http://api.weatherstack.com/current?access_key=d28ac7afbe74e9246448e2a4c74d2b23&query=37.8267,-122.4233&units=s';
 
-request( { url: url }, (error, response) => {
+request( { url: url, json: true }, (error, response) => {
   // console.log(response);
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+  // console.log(response.body.current);
+  console.log('The current temperature is:- ', response.body.current.temperature);
+  console.log('Temperature feels like:- ', response.body.current.feelslike);
+  console.log('The weather is ', response.body.current.weather_descriptions[0], ' type.');
 } );
