e9f473eb440ca768e91676140d177eabaf3d0aeb
Prateek Sarangi <sarangiprateek80@gmail.com>
2020-08-17 01:49:04 +0530

    Express codes added.

--- playground/3-es6objects.js
+++ playground/3-es6objects.js
@@ -1,1 +1,24 @@
 // Object property shorthand
+
+const name = 'Prateek';
+const age = 21;
+
+const user = {
+  name,
+  age,
+  location: 'Bargarh'
+};
+
+console.log(user);
+
+const product = {
+  label: 'My Notebook',
+  price: 3,
+  stock: 201,
+  salePrice: undefined
+};
+
+const { label, stock, price } = product;
+console.log(label);
+console.log(stock);
+console.log(price);
