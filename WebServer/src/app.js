10d8d19c7484562b357d2725668f304df8959e64
Prateek Sarangi <sarangiprateek80@gmail.com>
2020-09-17 11:59:03 +0530

    Handlebar for index page added.

--- WebServer/src/app.js
+++ WebServer/src/app.js
@@ -3,7 +3,12 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(path.join( __dirname, '../public' )));
+
+app.get ('/', ( req, res ) => {
+  res.render('index');
+});
 
 app.get ('/help', ( req, res ) => {
   res.send('Inside help page');
