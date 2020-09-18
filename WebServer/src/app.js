f180ade8ae3138408af0ae1eef417dbe1b7ab246
Prateek Sarangi <sarangiprateek80@gmail.com>
2020-09-18 10:18:46 +0530

    404 error page added

--- WebServer/src/app.js
+++ WebServer/src/app.js
@@ -1,26 +1,41 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.use(express.static(path.join( __dirname, '../public' )));
+const publicDir = path.join( __dirname, '../public' );
+const viewDir = path.join( __dirname, '../templates/views' );
+const partialDir = path.join( __dirname, '../templates/partials' );
+
+app.set ('view engine', 'hbs');
+app.set ('views', viewDir);
+hbs.registerPartials(partialDir);
+
+app.use (express.static(publicDir));
 
 app.get ('/', ( req, res ) => {
   res.render('index', {
     title: 'Weather',
+    head: 'Head of root page',
     name: 'Prateek'
   });
 });
 
 app.get ('/help', ( req, res ) => {
-  res.send('Inside help page');
+  res.render('help', {
+    message: 'This is the help page',
+    title: 'Help',
+    head: 'Head of the help page',
+    name: 'Prateek'
+  });
 });
 
 app.get ('/about', ( req, res ) => {
   res.render('about', {
     title: 'About',
-    head: 'About page HBS'
+    head: 'About page head',
+    name: 'Prateek'
   });
 });
 
@@ -28,6 +43,9 @@ app.get ('/weather', ( req, res ) => {
   res.send('Inside weather page');
 });
 
+app.get ('*', ( req, res ) => {
+  res.send('404 error page');
+});
 
 app.listen ( 5000, ( ) => {
   console.log('Server is running!');
